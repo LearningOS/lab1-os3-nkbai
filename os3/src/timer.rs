7da7fce5ce47c656ea313bfd7e4bfe37a7b8d3b9
baizhenxuan <baizhenxuan@bytedance.com>
2022-07-21 09:14:51 +0800

    lab1

--- os3/src/timer.rs
+++ os3/src/timer.rs
@@ -4,7 +4,7 @@ use riscv::register::time;
 
 const TICKS_PER_SEC: usize = 100;
 const MICRO_PER_SEC: usize = 1_000_000;
-
+const MILLI_PER_SEC: usize = 1_000;
 pub fn get_time() -> usize {
     time::read()
 }
@@ -12,7 +12,9 @@ pub fn get_time() -> usize {
 pub fn get_time_us() -> usize {
     time::read() / (CLOCK_FREQ / MICRO_PER_SEC)
 }
-
+pub fn get_time_milli() -> usize {
+    time::read() / (CLOCK_FREQ / MILLI_PER_SEC)
+}
 pub fn set_next_trigger() {
     set_timer(get_time() + CLOCK_FREQ / TICKS_PER_SEC);
 }
